252006dbe388becc40e08a75474ec7156cb8d706
bllnd <bllnd@mac.com>
2016-06-25 14:06:50 -0700

    s5

--- PHP/s5/functions.php
+++ PHP/s5/functions.php
@@ -1,5 +1,5 @@
 <?php
 function add($num1, $num2) {
-  return $num1 + $num2;
+  return $num1 + $num2.'<br />';
 }
 ?>
--- PHP/s5/index.php
+++ PHP/s5/index.php
@@ -17,3 +17,30 @@ include 'functions.php';
 echo add(7,2);
 ?>
 
+<?php
+$phrase = 'I like to eat apples';
+$phrase = str_replace('apples', 'oranges', $phrase);
+// echo $phrase;
+$name_array = array('Brad','Bob','Mike', 'Sarah','Michelle'.'<br />');
+// print_r($name_array);
+$name_str = implode(', ', $name_array);
+  echo $name_str;
+ ?>
+
+<?php
+$car_string = 'toyota, dodge, mazda, kia';
+$car_array = explode(', ', $car_string);
+print_r($car_array);
+ ?>
+
+ <?php
+class AreAnagrams
+{
+    public static function areStringsAnagrams($a, $b)
+    {
+        return count_chars($a,1) == count_chars($b,1);
+    }
+}
+
+echo AreAnagrams::areStringsAnagrams('neural', 'unreal') ? '<br />'.'True' : 'False';
+  ?>
