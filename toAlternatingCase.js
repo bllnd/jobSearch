10e51861259f4e9efa848530b12baf430edbca2b
bllnd <bllnd@mac.com>
2016-08-04 14:03:45 -0700

    added getRealFloor.js & toAlternatingCase.js

---
+++ toAlternatingCase.js
@@ -0,0 +1,24 @@
+String.prototype.toAlternatingCase = function() {
+  var strArr = this.split('');
+  for (var i = 0; i < strArr.length; i++) {
+
+    if (strArr[i] === strArr[i].toUpperCase()) {
+      strArr[i] = strArr[i].toLowerCase();
+    }
+    else {
+      strArr[i] = strArr[i].toUpperCase();
+    }
+  }
+    return strArr.join('');
+}
+
+// String.prototype.toInvertedCase = function () {
+//   tmp = "";
+//   for (i=0; i< this.length; i++) {
+//     t=this[i];
+//     t=(t==t.toUpperCase()?t.toLowerCase():t.toUpperCase()); tmp+=t;
+//   }
+//   return tmp;
+// }
+
+toAlternatingCase("Hello");
